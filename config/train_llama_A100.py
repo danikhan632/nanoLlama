6f3533f5acc9758f4727546e3384e616680dd61f
green <danikhan632@gmail.com>
2024-05-31 16:26:09 -0400

    adding A100 config

--- config/train_llama_A100.py
+++ config/train_llama_A100.py
@@ -5,8 +5,8 @@
 wandb_log = True
 wandb_project = 'owt'
 wandb_run_name='gpt2-124M'
-# dataset = 'openwebtext'
-dataset = 'shakespeare'
+dataset = 'openwebtext'
+
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 batch_size = 12
